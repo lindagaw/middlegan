9080f6ca8b4d35d6e2ff3a3ae58e9d80004c7c21
Ashley Gao <gaoye0718@gmail.com>
2022-04-15 13:10:05 -0400

    WIP

---
+++ eval.py
@@ -0,0 +1,39 @@
+import torch.nn as nn
+import torch.optim as optim
+
+def make_variable(tensor, volatile=False):
+    """Convert Tensor to Variable."""
+    if torch.cuda.is_available():
+        tensor = tensor.cuda()
+    return Variable(tensor, volatile=volatile)
+
+def evaluate(classifier, data_loader):
+    """Evaluate classifier for source domain."""
+    # set eval state for Dropout and BN layers
+
+    classifier.eval()
+
+    # init loss and accuracy
+    loss = 0.0
+    acc = 0.0
+
+    # set loss function
+    criterion = nn.CrossEntropyLoss()
+
+    # evaluate network
+    for (images, labels) in data_loader:
+        images = make_variable(images, volatile=True)
+        labels = make_variable(labels)
+
+        validities, preds = classifier(images)
+        loss += criterion(preds, labels).data
+
+        pred_cls = preds.data.max(1)[1]
+        acc += pred_cls.eq(labels.data).cpu().sum()
+
+    loss /= len(data_loader)
+    acc /= len(data_loader.dataset)
+
+    print("Avg Loss = {}, Avg Accuracy = {:2%}".format(loss, acc))
+
+    return acc
