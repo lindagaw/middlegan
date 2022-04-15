7de2440d14d356aac25615792d557714b2d9308c
Ashley Gao <gaoye0718@gmail.com>
2022-04-15 13:32:40 -0400

    WIP

--- eval.py
+++ eval.py
@@ -1,6 +1,7 @@
 import torch.nn as nn
 import torch.optim as optim
 import torch
+from torch.autograd import Variable
 
 def make_variable(tensor, volatile=False):
     """Convert Tensor to Variable."""
