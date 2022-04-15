83393b36e8ebf1b969e4738f1a9043a99e762f6a
Ashley Gao <gaoye0718@gmail.com>
2022-04-15 14:01:27 -0400

    WIP

--- acgan.py
+++ acgan.py
@@ -45,6 +45,39 @@ def weights_init_normal(m):
         torch.nn.init.constant_(m.bias.data, 0.0)
 
 
+norm_layer = nn.InstanceNorm2d
+class ResBlock(nn.Module):
+    def __init__(self, f):
+        super(ResBlock, self).__init__()
+        self.conv = nn.Sequential(nn.Conv2d(f, f, 3, 1, 1), norm_layer(f), nn.ReLU(),
+                                  nn.Conv2d(f, f, 3, 1, 1))
+        self.norm = norm_layer(f)
+    def forward(self, x):
+        return F.relu(self.norm(self.conv(x)+x))
+
+class Generator(nn.Module):
+    self.label_emb = nn.Embedding(opt.n_classes, opt.latent_dim)
+
+    def __init__(self, f=opt.img_size, blocks=6):
+        super(Generator, self).__init__()
+        layers = [nn.ReflectionPad2d(3),
+                  nn.Conv2d(  3,   f, 7, 1, 0), norm_layer(  f), nn.ReLU(True),
+                  nn.Conv2d(  f, 2*f, 3, 2, 1), norm_layer(2*f), nn.ReLU(True),
+                  nn.Conv2d(2*f, 4*f, 3, 2, 1), norm_layer(4*f), nn.ReLU(True)]
+        for i in range(int(blocks)):
+            layers.append(ResBlock(4*f))
+        layers.extend([
+                nn.ConvTranspose2d(4*f, 4*2*f, 3, 1, 1), nn.PixelShuffle(2), norm_layer(2*f), nn.ReLU(True),
+                nn.ConvTranspose2d(2*f,   4*f, 3, 1, 1), nn.PixelShuffle(2), norm_layer(  f), nn.ReLU(True),
+                nn.ReflectionPad2d(3), nn.Conv2d(f, 3, 7, 1, 0),
+                nn.Tanh()])
+        self.conv = nn.Sequential(*layers)
+
+    def def forward(self, noise, labels):
+        gen_input = torch.mul(self.label_emb(labels), noise)
+        return self.conv(gen_input)
+
+'''
 class Generator(nn.Module):
     def __init__(self):
         super(Generator, self).__init__()
@@ -74,7 +107,7 @@ class Generator(nn.Module):
         out = out.view(out.shape[0], opt.img_size, self.init_size, self.init_size)
         img = self.conv_blocks(out)
         return img
-
+'''
 
 class Discriminator(nn.Module):
     def __init__(self):
