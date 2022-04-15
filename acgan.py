e405ab32b0624fb3d2afcf339c45396070a19d71
Ashley Gao <gaoye0718@gmail.com>
2022-04-15 16:30:20 -0400

    wip

--- acgan.py
+++ acgan.py
@@ -178,13 +178,23 @@ os.makedirs("../../data/mnist", exist_ok=True)
 
 dataroot = "..//dcgan//datasets//office-31-intact//webcam//images//"
 
+import torchvision.transforms as T
+train_transforms = T.Compose([
+        T.Resize((256, 256)),
+        T.RandomCrop((224, 224)),
+        T.RandomHorizontalFlip(),
+        T.ToTensor(),
+        T.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+    ])
+val_transforms = T.Compose([
+    T.Resize((256, 256)),
+    T.CenterCrop((224, 224)),
+    T.ToTensor(),
+    T.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+])
+
 dataset = datasets.ImageFolder(root=dataroot,
-                           transform=transforms.Compose([
-                               transforms.Resize(opt.img_size),
-                               transforms.CenterCrop(opt.img_size),
-                               transforms.ToTensor(),
-                               transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
-                           ]))
+                           transform=train_transforms)
 # Set random seed for reproducibility
 manualSeed = 1999
 #manualSeed = random.randint(1, 10000) # use if you want new results
