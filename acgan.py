da6c2573e71fcc8a331f8d9ca4636877d40e4ac9
Ashley Gao <gaoye0718@gmail.com>
2022-04-18 16:04:01 -0400

    wip

--- acgan.py
+++ acgan.py
@@ -21,16 +21,16 @@ from eval import evaluate
 os.makedirs("images", exist_ok=True)
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--n_epochs", type=int, default=10, help="number of epochs of training")
+parser.add_argument("--n_epochs", type=int, default=100, help="number of epochs of training")
 parser.add_argument("--batch_size", type=int, default=64, help="size of the batches")
 parser.add_argument("--lr", type=float, default=0.0002, help="adam: learning rate")
 parser.add_argument("--b1", type=float, default=0.5, help="adam: decay of first order momentum of gradient")
 parser.add_argument("--b2", type=float, default=0.999, help="adam: decay of first order momentum of gradient")
 parser.add_argument("--n_cpu", type=int, default=8, help="number of cpu threads to use during batch generation")
 parser.add_argument("--latent_dim", type=int, default=100, help="dimensionality of the latent space")
-parser.add_argument("--n_classes", type=int, default=10, help="number of classes for dataset")
-parser.add_argument("--img_size", type=int, default=128, help="size of each image dimension")
-parser.add_argument("--channels", type=int, default=1, help="number of image channels")
+parser.add_argument("--n_classes", type=int, default=31, help="number of classes for dataset")
+parser.add_argument("--img_size", type=int, default=256, help="size of each image dimension")
+parser.add_argument("--channels", type=int, default=3, help="number of image channels")
 parser.add_argument("--sample_interval", type=int, default=40, help="interval between image sampling")
 opt = parser.parse_args()
 print(opt)
@@ -172,11 +172,11 @@ generator.apply(weights_init_normal)
 #discriminator.apply(weights_init_normal)
 
 # Configure data loader
-os.makedirs("data/mnist", exist_ok=True)
+os.makedirs("../../data/mnist", exist_ok=True)
 
 dataroot = "..//dcgan//datasets//office-31-intact//webcam//images//"
 
-'''
+
 dataset = datasets.ImageFolder(root=dataroot,
                            transform=transforms.Compose([
                                transforms.Resize(opt.img_size),
@@ -188,34 +188,7 @@ dataset = datasets.ImageFolder(root=dataroot,
 train_set, test_set = torch.utils.data.random_split(dataset, [int(len(dataset)*0.8), len(dataset)-int(len(dataset)*0.8)])
 dataloader = torch.utils.data.DataLoader(train_set, batch_size=opt.batch_size, shuffle=True)
 dataloader_test = torch.utils.data.DataLoader(test_set, batch_size=opt.batch_size, shuffle=True)
-'''
 
-dataloader = torch.utils.data.DataLoader(
-    datasets.MNIST(
-        "data/mnist",
-        train=True,
-        download=True,
-        transform=transforms.Compose(
-            [transforms.Grayscale(3), transforms.Resize(opt.img_size), transforms.ToTensor(), transforms.Normalize([0.5], [0.5])]
-        ),
-    ),
-    batch_size=opt.batch_size,
-    shuffle=True,
-)
-
-dataloader_test = torch.utils.data.DataLoader(
-    datasets.MNIST(
-        "data/mnist",
-        train=False,
-        download=True,
-        transform=transforms.Compose(
-            [transforms.Grayscale(3), transforms.Resize(opt.img_size), transforms.ToTensor(), transforms.Normalize([0.5], [0.5])]
-        ),
-    ),
-    batch_size=opt.batch_size,
-    shuffle=True,
-
-)
 
 # Optimizers
 optimizer_G = torch.optim.Adam(generator.parameters(), lr=opt.lr, betas=(opt.b1, opt.b2))
